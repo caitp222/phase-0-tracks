c37e6a772642b1cd00e5a7e5076b9606bacddde8
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-10 15:59:17 -0400

    Add characteristics and description of the Puppy species.

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES
+#   Puppyus
+
+# CHARACTERISTICS
+#   Eye Size: Huge
+#   Body Size: Tiny
+#   Leg Count: 4
+#   Name: Varies
+#   Color: Varies
+#   Coat: Soft
+
+# BEHAVIOR
+#   Cuddle
+#   Runs Around
+#   Collapses into sleep
