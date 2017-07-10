27a53449c30bd398f802f35aace5eb3a2b5ba8b5
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-10 10:01:44 -0400

    Add code for iteration exercises

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -81,14 +81,30 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
+extinct_animals.each do |animal, year|
+  puts "#{animal}-#{year}*"
+end
+
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000, using #each.
 # ----
+
+extinct_animals.keep_if {|animal, year|
+  year < 2000}
+
+p extinct_animals
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # using #each, so they accurately reflect what year the animal went extinct.
 # ----
+
+extinct_animals.each {
+  |animal, year|
+  extinct_animals[animal] = year - 3
+}
+
+p extinct_animals
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Build a method  using #each that checks if an animal is in the hash and returns true/false.
@@ -100,8 +116,27 @@ extinct_animals = {
 # Driver code example: is_extinct?(extinct_animals, "Andean Cat")
 # ----
 
+# Easier method (built-in):
+def extinction_checker(hash, animal)
+  hash.has_key?(animal)
+end
+
+p extinction_checker(extinct_animals, "Dodo")
+p extinction_checker(extinct_animals, "Andean Cat")
+p extinction_checker(extinct_animals, "Saiga Antelope")
+
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find a Ruby Hash built-in method that helps you accomplish this or build
 # your own method using #each
 # ----
+
+
+p extinct_animals.assoc("Passenger Pigeon")
+
+extinct_animals.delete("Passenger Pigeon")
+
+p extinct_animals
+
+
