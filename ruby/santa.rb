afe91f512116cb02d84775c15da1a4f4ffa21d17
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-11 19:41:00 -0400

    Add an #each loop to create new santas from a hash

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    #@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    #@age = 0
   end
 
   def speak
@@ -14,7 +18,41 @@ class Santa
 
 end
 
-santa_claus = Santa.new
+santa_claus = Santa.new("Female","White")
 
 santa_claus.speak
 santa_claus.eats_milk_and_cookies("snickerdoodle")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# p santas
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i],example_ethnicities[i])
+# end
+
+# p santas
+
+santas = []
+examples = {
+  genders: ["female", "gender non-conforming", "intersex", "male", "more than one", "none"],
+  ethnicities: ["elf", "dwarf", "orc", "human", "none", "all"]
+}
+
+
+examples[:genders].length.times do |i|
+  santas << Santa.new(examples[:genders][i], examples[:ethnicities][i])
+end
+
+p santas
