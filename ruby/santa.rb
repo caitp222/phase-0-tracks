9eb8638f3173f9aeb31234bb4d5a2bffe66e745e
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-11 17:27:29 -0400

    Add code for release 0, three santa instance methods.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,20 @@
-class santa
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
 
   def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
 
+  def eats_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!!"
   end
 
 end
+
+santa_claus = Santa.new
+
+santa_claus.speak
+santa_claus.eats_milk_and_cookies("snickerdoodle")
