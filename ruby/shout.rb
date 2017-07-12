3d2a0e44b2e64ee2f156387b365f1ab1cc57f96b
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-12 16:53:44 -0400

    Add mixin module code for Shout, and driver code to test it on instances of two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,55 @@
-#MODULE DECLARATION
+# #MODULE DECLARATION
+
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     ":D " + words +"!!!!"
+#   end
+
+# end
+
+# #DRIVER CODE
+
+# p Shout.yell_angrily("Go Away")
+
+# p Shout.yelling_happily("Hello")
+
+
+#MIXIN MODULE DECLARATION
+
+#Create Mixin module for Shout module declared above
+#Create two classes that might shout
+#Call the two Shout methods on new instances of the two classes
 
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     ":D " + words +"!!!!"
   end
 
 end
 
-#DRIVER CODE
+#CLASS DECLARATIONS
 
-p Shout.yell_angrily("Go Away")
+class Disgruntled_employees
+    include Shout
+  end
 
-p Shout.yelling_happily("Hello")
+class Wedding_guests
+    include Shout
+  end
+
+disgruntled_employee = Disgruntled_employees.new
+wedding_guest = Wedding_guests.new
+
+p disgruntled_employee.yell_angrily("I quit")
+
+p wedding_guest.yelling_happily("Congratulations")
