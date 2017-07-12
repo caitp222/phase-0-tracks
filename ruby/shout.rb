f92dd521383ae184ddf7dc59a20b963c9b977148
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-12 16:42:01 -0400

    Add module and driver code for yell_angrily and yelling_happily

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,19 @@
+#MODULE DECLARATION
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    ":D " + words +"!!!!"
+  end
+
 end
+
+#DRIVER CODE
+
+p Shout.yell_angrily("Go Away")
+
+p Shout.yelling_happily("Hello")
