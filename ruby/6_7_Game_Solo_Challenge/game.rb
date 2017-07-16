88d89b796b941d95a45baff3be54ad80b359f408
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-16 16:12:22 -0400

    Adjust code so # of guesses remaining isn't printed after the word is correctly guessed

--- ruby/6_7_Game_Solo_Challenge/game.rb
+++ ruby/6_7_Game_Solo_Challenge/game.rb
@@ -1,12 +1,11 @@
 # PSEUDOCODE FOR WORD GUESSING GAME
 
 # When user1 enters a word to be guessed, initalize a new instance of Guessword class
-# Within initialize method, split word into array1
+# Within initialize method, split word into array1 [parameter: string, return value: array]
 # Define a guesscount method [parameter: string, return value: integer] that returns a guesscount value of the number of letters in the array + 5
-# Define a guess_array method [parameter: string, return value: array] that creates a new array2 where all elements are "-" and length is equal to array1.length
-# Define a compare method [parameter: string, return value: array] that takes user2's letter and loops over array1 to find index value of matches. Inserts those values into array2 at matching indexes.
-# Set array2 variable to be the array returned by compare method
-# Loop until guesscount is 0 or there are no instances of "-" in Array2
+# Define a guess_array method [parameter: none, return value: array] that creates a new array2 where all elements are "-" and length is equal to array1.length
+# Define a compare method [parameter: string, return value: array] that takes user2's letter and iterates over array1 to find index value of matches. Inserts those values into array2 at matching indices.
+# Create a loop so user2 can enter guesses until guesscount is 0 or there are no instances of "-" in Array2
     #Add each guess to an array, and use a conditional that only adds to the guesscount if the new guess is not already in the array
 # Using conditionals, if guesscount reaches 0 before the word is guessed, display sarcastic message. If the word is guessed, display congratulatory message
 
@@ -14,8 +13,7 @@
 # GAME CLASS DECLARATION
 
 class Guessword
-  attr_reader :guessword_array
-  attr_accessor :guessed_array
+  attr_reader :guessword_array, :guessed_array
 
   def initialize(word)
     @guessword_array = word.chars
@@ -53,8 +51,8 @@ puts "Hello! Please enter a word for your friend to guess!"
 game_word = Guessword.new(gets.chomp)
 game_word.guess_array
 guess_counter = game_word.guess_count
-
 user_guesses = []
+
 puts "Now, let your friend guess letters in the word! Warning: guesses are limited!"
 
     until guess_counter == 0 || !game_word.guessed_array.include?("-")
@@ -65,12 +63,15 @@ puts "Now, let your friend guess letters in the word! Warning: guesses are limit
           puts "That letter's not in the word!"
           user_guesses << letter
           guess_counter -= 1
+          puts "#{guess_counter} guesses remaining!"
         elsif  game_word.guessword_array.include?(letter)
           puts "Correct!"
           p game_word.compare(letter).join
           user_guesses << letter
           guess_counter -= 1
-          puts "#{guess_counter} guesses remaining!"
+            if game_word.guessed_array.include?("-")
+              puts "#{guess_counter} guesses remaining!"
+            end
         end
     end
 
