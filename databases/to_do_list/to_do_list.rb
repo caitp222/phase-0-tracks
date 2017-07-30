d248011593bf7193b96ed5185d761a5733946d4b
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-07-30 09:51:53 -0400

    Add driver code to add tasks to the to do list.

--- databases/to_do_list/to_do_list.rb
+++ databases/to_do_list/to_do_list.rb
@@ -4,7 +4,7 @@
 # create an interface so user can add a task to the database
 # allow user to retrieve tasks by due date or by their name
 
-# Install and require SQLite gem
+# Install and require SQLite gem and dates
 require 'sqlite3'
 
 # Create database
@@ -13,17 +13,34 @@ db.results_as_hash = true
 
 # create new table if it doesn't exist already
 create_new_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS prentke_to_do_list(
+  CREATE TABLE IF NOT EXISTS prentke_list(
     id INTEGER PRIMARY KEY,
     user_name VARCHAR(255),
-    due_date DATE,
     task VARCHAR(500)
   )
 SQL
 
+# create new row in table using user entered data
+def create_new_task (db, names, tasks)
+  db.execute("INSERT INTO prentke_list (user_name, task)
+    VALUES (?, ?)", [names, tasks])
+end
+
+def display_tasks(db, name)
+  db.execute("SELECT * FROM prentke_list WHERE user_name=#{name}")
+end
+
 db.execute(create_new_table)
 
+# DRIVER CODE
 
+# Create new tasks
+create_new_task(db, "Nancy", "Do the dishes")
+create_new_task(db, "Brian", "Make dinner")
+create_new_task(db, "Nancy", "Get the car cleaned")
+create_new_task(db, "Walker", "Go grocery shopping")
+create_new_task(db, "Caitlin", "Take the cats to the vet")
 
+# Display only Nancy's tasks
+# display_tasks(db, "Nancy")
 
-# USER INTERFACE
