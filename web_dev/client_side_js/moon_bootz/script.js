fd3d510313fff0947e55c83ec40ffab55d44b179
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-08-06 13:12:05 -0400

    Add option to hide image and show it when text is clicked

--- web_dev/client_side_js/moon_bootz/script.js
+++ web_dev/client_side_js/moon_bootz/script.js
@@ -1,7 +1,10 @@
-console.log("this script is here!");
+// Test
+console.log("This script is here!");
 
+// Add border to the image when text is clicked
 function addborder() {
   document.getElementById("boots_image").style.border = "3px solid yellow";
+  // document.getElementById("boots_image").hidden = false;
 }
 
 var faveboots = document.getElementById("favoriteboots");
